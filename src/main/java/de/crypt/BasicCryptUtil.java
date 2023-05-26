00aea28e95409b5a1c246312af58f166824587e9
xxx <xxx@web.de>
2023-05-26 21:36:18 +0200

    BasicCryptUtil in CommandLineRunner started

--- src/main/java/de/crypt/BasicCryptUtil.java
+++ src/main/java/de/crypt/BasicCryptUtil.java
@@ -2,18 +2,27 @@ package de.crypt;
 
 import lombok.Data;
 import org.jasypt.util.text.BasicTextEncryptor;
+import org.springframework.stereotype.Component;
+
 @Data
+@Component
 public class BasicCryptUtil {
     private static final String PASSWORD_DEFAULT = "secret";
-    public final BasicTextEncryptor basicTextEncryptor;
-    public String encryptText(String password, String text){
-        if(password.isBlank()) password = PASSWORD_DEFAULT;
-        basicTextEncryptor.setPassword(password);
+    public BasicTextEncryptor basicTextEncryptor; // = new BasicTextEncryptor();
+    public String password;
+    public BasicCryptUtil(){}
+    public BasicCryptUtil(BasicTextEncryptor basicTextEncryptor, String password){
+        this.basicTextEncryptor = basicTextEncryptor;
+        if(password.isBlank()){
+            this.basicTextEncryptor.setPassword(PASSWORD_DEFAULT);
+        }else{
+            this.basicTextEncryptor.setPassword(password);
+        }
+    }
+    public String encryptText(String text){
         return basicTextEncryptor.encrypt(text);
     }
-    public String decryptText(String password, String text){
-        if(password.isBlank()) password = PASSWORD_DEFAULT;
-        basicTextEncryptor.setPassword(password);
+    public String decryptText(String text){
         return basicTextEncryptor.decrypt(text);
     }
 }
--- src/main/java/de/crypt/CryptApplication.java
+++ src/main/java/de/crypt/CryptApplication.java
@@ -1,13 +1,37 @@
 package de.crypt;
 
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CryptApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CryptApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner runCLI(){
+		return (args) -> {
+			String pwd = "secret";
+
+			BasicTextEncryptor bte = new BasicTextEncryptor();
+			BasicCryptUtil basicCryptUtil1 = new BasicCryptUtil(bte, pwd);
+			basicCryptUtil1.setBasicTextEncryptor(bte);
+			basicCryptUtil1.setPassword(pwd);
+
+			String plaintext = "plaintext!";
+			String enctext = basicCryptUtil1.encryptText(plaintext);
+			String dectext = basicCryptUtil1.decryptText(enctext);
+
+			String msg = String.format("PASSWORD: %s%nPLAINTEXT: %s%nENCRYPTED: %s%nDECRYPTED: %s",
+					pwd, plaintext, enctext, dectext);
+
+			System.out.println("\n==================================\n");
+			System.out.println(msg);
+		};
 	}
 
 }
